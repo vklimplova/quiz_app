b5aa179cb90f53ffb32e51560c5194fb6735710d
veruklimpl <veronika.klimplova@seznam.cz>
2023-12-16 09:50:52 +0100

    new welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
 
   return (
   <div>
-    <div className="header">Vítejte</div>
+    <div className="header">Vítejte kamarádi</div>
     <Quiz questions={questions} />
   </div>
   )
